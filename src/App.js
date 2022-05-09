d8ff4420fd2374423b50f047af935e57331f3473
Lana Sharin <lana.sharin.webdev@gmail.com>
2022-05-08 21:01:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      person: null,
+    }
+  }
+
+  async componentDidMount() {
+      const url = 'https://api.randomuser.me';
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({
+        person: data.results[0],
+      })
+       
+
+      console.log(data.results[0].gender)
+  }
+
+  render() {
+    return (
+      <div className='App-main-container'>
+        {/* Если профайл еще не подгрузился, то покажи Loading...
+        Если инфа получена, то выведи её на экран */}
+          {!this.state.person ? <p>Loading...</p> : 
+
+          <div className='App-container'>
+
+          <img className='App-image' src={this.state.person.picture.large} alt='userPic'/>
+          <p className='App-name'>{this.state.person.name.first} 
+          <span> {this.state.person.name.last}</span></p>
+          <p className='App-gender'>Gender: {this.state.person.gender}</p>
+          <p>{this.state.person.email}</p> 
+
+          </div>
+
+          }
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
